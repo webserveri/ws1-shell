124d2d6e8ba935c24b9c3d757b27c23a2ffb159e
Zlatan Vasović <legospace9876@gmail.com>
2013-10-21 16:41:07 +0200

    Log ide u /tmp/

--- src/jpg-resize.sh
+++ src/jpg-resize.sh
@@ -2,4 +2,4 @@
 #
 # Smanjuje JPG slike u trenutnom direktorijumu.
 
-for i in *.jpg; do convert $i -resize 200 $i 2> /dev/null; done
+for i in *.jpg; do convert $i -resize 200 $i > /tmp/jpg-resize.log; done
