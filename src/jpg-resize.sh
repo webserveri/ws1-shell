ec365c6177557c1f268d47b601a73dde7f3a19cb
Zlatan Vasović <legospace9876@gmail.com>
2013-11-04 07:55:08 +0100

    Podela skripte na više linija

--- src/jpg-resize.sh
+++ src/jpg-resize.sh
@@ -2,4 +2,7 @@
 #
 # Smanjuje JPG slike u trenutnom direktorijumu.
 
-for i in *.jpg; do convert $i -resize 250 $i > /tmp/jpg-resize.log; done
+for i in *.jpg
+do
+  convert $i -resize 250 $i > /tmp/jpg-resize.log
+done
